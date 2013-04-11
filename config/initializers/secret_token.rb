d27282ffc60425ce7565872d4fab785851649daf
firstname.lastname <first.last@icicletech.com>
2013-04-11 11:47:13 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Incon::Application.config.secret_token = 'ca4cd656f8f3f8acf4acb57c703cdaaebf0ca53b27e10a2222f2a6a9c0a21e3f5c1d71bcff33bf2985e093ba3cdc41baf6f3c0773ceef5cc2fdc5b2f5b869d5f'
