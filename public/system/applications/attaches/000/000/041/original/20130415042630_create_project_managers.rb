13ceb77b9c6646bf8797b452f32a8e2a1b368569
Nishanth <nishanth@nishanth-OptiPlex-990.(none)>
2013-04-24 17:03:38 +0530

    Update attributes in applications

--- public/system/applications/attaches/000/000/041/original/20130415042630_create_project_managers.rb
+++
@@ -1,9 +0,0 @@
-class CreateProjectManagers < ActiveRecord::Migration
-  def change
-    create_table :project_managers do |t|
-      t.string :project_name
-
-      t.timestamps
-    end
-  end
-end
