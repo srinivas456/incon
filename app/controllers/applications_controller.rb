cfe09a67368ffa10a5f19d7232236ac8c38670b4
Srinivas Elluri <srinivas.elluri@mobiloustech.com>
2013-04-29 10:13:39 +0530

    Edit functionality solve issue

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -31,8 +31,16 @@ class ApplicationsController < ApplicationController
   end
 
   # GET /applications/1/edit
+  # def edit
+  #   @application = Application.find_by_id(params[:id])
+  # end
+
   def edit
+    session[:application_params] ||= params[:application_id]
+
     @application = Application.find_by_id(params[:id])
+    @application.current_step = session[:application_step]
+
   end
 
   # POST /applications
@@ -62,20 +70,44 @@ class ApplicationsController < ApplicationController
 
   # PUT /applications/1
   # PUT /applications/1.json
-  def update
-    @application = Application.find(params[:id])
-    #@application = Application.find_by_project_manager_id(params[:project_manager_id])
+  # def update
+  #   #@application = Application.find(params[:id])
+  #   @application = Application.find_by_project_manager_id(params[:project_manager_id])
 
-    respond_to do |format|
-      if @application.update_attributes(params[:application])
-        format.html { redirect_to @application, notice: 'Project manager was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @application.errors, status: :unprocessable_entity }
-      end
-    end
+  #   respond_to do |format|
+  #     if @application.update_attributes(params[:application])
+  #       format.html { redirect_to @application, notice: 'Project manager was successfully updated.' }
+  #       format.json { head :no_content }
+  #     else
+  #       format.html { render action: "edit" }
+  #       format.json { render json: @application.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+
+   def update
+    session[:application_params].deep_merge!(params[:application]) if params[:application]
+    @application = Application.find(params[:id])
+    @application.current_step = session[:application_step]
+    if @application.valid?
+    if params[:back_button]
+        @application.previous_step
+  elsif @application.last_step?
+      @application.update_attributes(params[:application]) if @application.all_valid?
+    else
+     @application.next_step 
+   end
+    session[:application_step] = @application.current_step
   end
+    if @application.save
+      render "new"
+    else
+      session[:application_step] = session[:application_params] = nil
+    flash[:notice] = "application saved!"
+      redirect_to @application
+    end
+    end
 
   # DELETE /applications/1
   # DELETE /applications/1.json
